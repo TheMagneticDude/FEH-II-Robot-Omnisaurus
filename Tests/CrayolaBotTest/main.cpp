74df4360505235ee3db29feecacad0979a70ad68
TheMagneticDude <110270128+TheMagneticDude@users.noreply.github.com>
2025-02-07 15:35:31 -0500

    Craola bot kinda works

--- Tests/CrayolaBotTest/main.cpp
+++ Tests/CrayolaBotTest/main.cpp
@@ -1,8 +1,61 @@
 #include <FEHLCD.h>
+#include <FEHLCD.h>
+#include <FEHIO.h>
+#include <FEHUtility.h>
+#include <FEHMotor.h>
+#include <FEHServo.h>
+#include <FEHAccel.h>
+#include <FEHBattery.h>
+#include <FEHBuzzer.h>
+#include <FEHRCS.h>
+#include <FEHSD.h>
+#include <string.h>
+#include <stdio.h>
 
 int main(void)
 {
+
+    DigitalInputPin LimitFrontLeft(FEHIO::P3_0);
+    DigitalInputPin LimitFrontRight(FEHIO::P3_1);
+    DigitalInputPin LimitBackLeft(FEHIO::P3_4);
+    DigitalInputPin LimitBackRight(FEHIO::P3_5);
+
+    FEHMotor right_motor(FEHMotor::Motor0,9.0);
+    FEHMotor left_motor(FEHMotor::Motor1, 9.0);
+    while(1){
     LCD.Clear(BLACK);
-    LCD.WriteLine("Hello, World!");
+    LCD.WriteLine("WEEE");
+
+    float speed = 40.0;
+
+    if(!LimitFrontLeft.Value()){
+        right_motor.SetPercent(speed);
+        LCD.WriteLine("FRONT LEFT");
+    }else{
+        right_motor.SetPercent(0);
+    }
+
+    if(!LimitFrontRight.Value()){
+        left_motor.SetPercent(speed);
+        LCD.WriteLine("FRONT RIGHT");
+    }else{
+        left_motor.SetPercent(0);
+    }
+
+    if(!LimitBackLeft.Value()){
+        right_motor.SetPercent(-speed);
+        LCD.WriteLine("BACK LEFT");
+    }else{
+        right_motor.SetPercent(0);
+    }
+
+    if(!LimitBackRight.Value()){
+        left_motor.SetPercent(-speed);
+        LCD.WriteLine("BACK RIGHT");
+    }else{
+        left_motor.SetPercent(0);
+    }
+    
+    }
 	return 0;
 }
