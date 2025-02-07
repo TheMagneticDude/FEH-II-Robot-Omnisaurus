43c94cb194549acb957b1309a7f5e726ce8433bb
TheMagneticDude <110270128+TheMagneticDude@users.noreply.github.com>
2025-02-07 14:49:22 -0500

    Crayola bot

--- RobotProject/main.cpp
+++ RobotProject/main.cpp
@@ -1,5 +1,10 @@
 #include <FEHLCD.h>
-
+#include <Windows.h>
+//gamepad
+#include <concrt.h>
+#include <winrt/Windows.Gaming.Input.h>
+using namespace winrt;
+using namespace Windows::Gaming::Input;
 int main(void)
 {
     LCD.Clear(BLACK);
