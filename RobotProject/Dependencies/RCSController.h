da79e05331198dcf36e75168b0a948d7e2fa4f18
TheMagneticDude <110270128+TheMagneticDude@users.noreply.github.com>
2025-02-10 15:13:05 -0500

    hardcoding crayola bot

--- RobotProject/Dependencies/RCSController.h
+++ RobotProject/Dependencies/RCSController.h
@@ -2,7 +2,7 @@
 
 #include <FEHLCD.h>
 #include <string>
-
+#include <FEHXBee.h>
 #include "Input.h"
 
 using namespace std;
--- Tests/CrayolaBotTest/main.cpp
+++ Tests/CrayolaBotTest/main.cpp
@@ -15,57 +15,119 @@ int main(void)
 {
 
     DigitalInputPin LimitFrontLeft(FEHIO::P3_0);
-    DigitalInputPin LimitFrontRight(FEHIO::P3_1);
-    DigitalInputPin LimitBackLeft(FEHIO::P3_4);
-    DigitalInputPin LimitBackRight(FEHIO::P3_5);
+    DigitalInputPin LimitFrontRight(FEHIO::P0_0);
+    DigitalInputPin LimitBackLeft(FEHIO::P3_1);
+    DigitalInputPin LimitBackRight(FEHIO::P0_1);
 
-    FEHMotor right_motor(FEHMotor::Motor0,9.0);
-    FEHMotor left_motor(FEHMotor::Motor1, 9.0);
-    while(1){
+    FEHMotor right_motor(FEHMotor::Motor1,9.0);
+    FEHMotor left_motor(FEHMotor::Motor2, 9.0);
+
+    // while(1){
+    // LCD.Clear(BLACK);
+    // LCD.WriteLine("WEEE");
+
+    // float speed = 40.0;
+    // if(!LimitFrontLeft.Value() && !LimitFrontRight.Value()){
+    //     LCD.WriteLine("FRONT TWO PRESSED");
+    //     //if both are pressed, move backwards first
+    //     left_motor.SetPercent(-speed);
+    //     right_motor.SetPercent(-speed);
+    //     Sleep(1000);
+    //     //turn right
+        
+    //     left_motor.SetPercent(speed);
+    //     right_motor.SetPercent(-speed);
+    //     Sleep(1000);
+    // }else{
+
+    //     //front switches
+    // //move forward if switches not toggled
+    // if(LimitFrontLeft.Value()){
+    //     left_motor.SetPercent(speed);
+    // }else{
+    //     LCD.WriteLine("Front Left PRESSED");
+    //     //move backwards first
+    //     left_motor.SetPercent(-speed);
+    //     right_motor.SetPercent(-speed);
+    //     Sleep(1000);
+    //     //move backwards if switches toggled
+    //     left_motor.SetPercent(speed);
+    //     Sleep(1000);
+    // }
+
+    //     if(LimitFrontRight.Value()){
+    //         right_motor.SetPercent(speed);
+    //     }else{
+    //         LCD.WriteLine("Front Right PRESSED");
+    //         left_motor.SetPercent(-speed);
+    //         right_motor.SetPercent(-speed);
+    //         Sleep(1000);
+    //         //move backwards if switches toggled
+    //         right_motor.SetPercent(speed);
+    //         Sleep(1000);
+    //     }
+
+    // }
+
+
+
+    
+
+
+
+    
+
+    
+    
+    
+    // }
+
     LCD.Clear(BLACK);
     LCD.WriteLine("WEEE");
-
+    //hardcoded
     float speed = 40.0;
-    //front switches
-    //move forward if switches not toggled
-    if(LimitFrontLeft.Value()){
+    //forwards
+    while(LimitFrontLeft.Value() || LimitFrontRight.Value()){
+        LCD.WriteLine("Init move forwards");
         left_motor.SetPercent(speed);
-    }
- 
-    if(LimitFrontRight.Value()){
         right_motor.SetPercent(speed);
     }
-
-//move backwards if switches toggled
-    if(!LimitFrontLeft.Value()){
+        LCD.WriteLine("FRONT TWO PRESSED");
+        //if both are pressed, move backwards first
         left_motor.SetPercent(-speed);
-    }
-
-    if(!LimitFrontRight.Value()){
         right_motor.SetPercent(-speed);
-    }
+        Sleep(500);
+        //turn right
+        LCD.WriteLine("Turning right");
+        left_motor.SetPercent(-speed);
+        right_motor.SetPercent(speed);
+        if(!LimitFrontLeft.Value() || !LimitFrontRight.Value()){
+            LCD.WriteLine("FRONT TWO PRESSED AGAIN");
+            while(!LimitFrontLeft.Value() && !LimitFrontRight.Value()){
+                left_motor.SetPercent(-speed);
+                right_motor.SetPercent(-speed);
+                Sleep(500);
+                LCD.WriteLine("Turning Left");
+                left_motor.SetPercent(speed);
+                right_motor.SetPercent(-speed);
+            }
+            
+        }
+
+        if(!LimitFrontLeft.Value() || !LimitFrontRight.Value()){
+            LCD.WriteLine("FRONT TWO PRESSED THIRD TIME");
+            while(!LimitFrontLeft.Value() && !LimitFrontRight.Value()){
+                left_motor.SetPercent(-speed);
+                right_motor.SetPercent(-speed);
+                Sleep(500);
+                LCD.WriteLine("Turning Left");
+                left_motor.SetPercent(speed);
+                right_motor.SetPercent(-speed);
+            }
+            
+        }
+
+
     
-
-    //back switches
-    //move forward if switches not toggled
-//     if(LimitBackLeft.Value()){
-//         left_motor.SetPercent(speed);
-//     }
-
-//     if(LimitBackRight.Value()){
-//         right_motor.SetPercent(speed);
-//     }
-
-// //move backwards if switches toggled
-//     if(!LimitBackLeft.Value()){
-//         left_motor.SetPercent(-speed);
-//     }
-
-//     if(!LimitBackRight.Value()){
-//         right_motor.SetPercent(-speed);
-//     }
-    
-    
-    }
 	return 0;
 }
