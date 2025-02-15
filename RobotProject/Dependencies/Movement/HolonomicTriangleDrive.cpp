e078ffd8f030e4482c9875c3584291ce791c6b3c
TheMagneticDude <TheMagneticDude@gmail.com>
2025-02-15 04:50:28 -0500

    Holonomic Triangle Drive
    Holonomic Triangle Drive started

--- RobotProject/Dependencies/Movement/HolonomicTriangleDrive.cpp
+++ RobotProject/Dependencies/Movement/HolonomicTriangleDrive.cpp
@@ -1,9 +1,81 @@
+#include "HolonomicTriangleDrive.h"
 #include <limits>
 #include <string>
-
-#include "Button.h"
-#include "Sound.h"
 #include <FEHLCD.h>
 
 using namespace std;
+
+
+float dot(float a[], float b[]){
+    return a[0] * b[0] + a[1] * b[1];
+}
+
+float magnitude(float v[]){
+    return std::sqrt((v[0] * v[0]) + (v[1] * v[1]));
+}
+
+//Projection magnitude of vector `a` onto vector `v`
+//Gives magnitude of the projected 2D vector
+//requires v and a are 2D vectors
+float proj(float v[], float a[]){
+    float vMag = magnitude(v);
+    if(vMag == 0){
+        return 0; //no divide by 0 error plz
+    }
+    return ( dot(a,v) / (vMag) );
+}
+
+float clamp(float x, float min, float max){
+    if(x <= min){
+        return min;
+    }
+    if(x >= max){
+        return max;
+    }
+    return x;
+}
+
+HolonomicTriangleDrive::HolonomicTriangleDrive(FEHMotor::FEHMotorPort F, FEHMotor::FEHMotorPort BL, FEHMotor::FEHMotorPort BR) 
+: Front(F, defaultMotorMaxVolt), 
+BackLeft(BL, defaultMotorMaxVolt),
+BackRight(BR,defaultMotorMaxVolt)
+{
+    FrontPort = F;
+    BackLeftPort = BL;
+    BackRightPort = BR;
+}
+
+//sets target movement vector
+void HolonomicTriangleDrive::setMovementVector(float x, float y){
+    MovementVector[0] = x;
+    MovementVector[1] = y;
+}
+
+//Updates all motor states every cycle
+void HolonomicTriangleDrive::update(){
+    float fSpd = proj(M1, MovementVector);
+    float blSpd = proj(M2, MovementVector);
+    float brSpd = proj(M3, MovementVector);
+
+    //get max speed out of three motors
+
+    float maxSpeed = std::max(fabs(fSpd), std::max(fabs(blSpd), fabs(brSpd)));
+
+    //scales speed down so the movement vector is still correct
+    if (maxSpeed > maxSpeedPercent) {
+        fSpd *= (maxSpeedPercent / maxSpeed);
+        blSpd *= (maxSpeedPercent / maxSpeed);
+        brSpd *= (maxSpeedPercent / maxSpeed);
+    }
+
+    FrontSped = clamp(fSpd,-maxSpeed,maxSpeed);
+    BackLeftSped = clamp(blSpd,-maxSpeed,maxSpeed);
+    BackRightSped = clamp(brSpd,-maxSpeed,maxSpeed);
+
+    Front.SetPercent(FrontSped * 100);
+    BackLeft.SetPercent(BackLeftSped * 100);
+    BackRight.SetPercent(BackRightSped * 100);
+}
+
+
 
--- RobotProject/Dependencies/Movement/HolonomicTriangleDrive.h
+++ RobotProject/Dependencies/Movement/HolonomicTriangleDrive.h
@@ -12,6 +12,7 @@
 #include <FEHSD.h>
 #include <string.h>
 #include <stdio.h>
+#include <cmath>
 
 
 
@@ -29,7 +30,7 @@ class HolonomicTriangleDrive {
 //       \   /
 //        O
  
-    FEHMotor Front;
+    FEHMotor Front;//M1
 
 
 //BackLeft
@@ -41,7 +42,7 @@ class HolonomicTriangleDrive {
 //       \   /
 //        x
 
-    FEHMotor BackLeft;
+    FEHMotor BackLeft;//M2
 
 
 
@@ -53,12 +54,36 @@ class HolonomicTriangleDrive {
 //      \     /
 //       \   /
 //        0
-    FEHMotor BackRight;
+    FEHMotor BackRight;//M3
+
+
+
+    //motor vector directions
+    float M1[2] = {1,0};
+    float M2[2] = {-(std::sqrt(3) / 2.0),-0.5};
+    float M3[2] = {(std::sqrt(3) / 2.0),-0.5};
+    float MovementVector[2] = {0,0};
 
     
   public:
   FEHMotor::FEHMotorPort FrontPort;
   FEHMotor::FEHMotorPort BackLeftPort;
   FEHMotor::FEHMotorPort BackRightPort;
+
+  float FrontSped = 0;
+  float BackLeftSped = 0;
+  float BackRightSped = 0;
+
+  const float defaultMotorMaxVolt = 9.0;
+
+  //max speed percentage
+  const float maxSpeedPercent = 1;
+
+  HolonomicTriangleDrive::HolonomicTriangleDrive(FEHMotor::FEHMotorPort Front, FEHMotor::FEHMotorPort BackLeft, FEHMotor::FEHMotorPort BackRight);
+  void setMovementVector(float x, float y);
+  void update();
+
+
+  
     
 };
